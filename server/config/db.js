714c4f44c3badd0bd3afdc0b024c766297139067
kevinburnside <kevinburnside15@hotmail.com>
2017-02-23 17:23:41 -0500

    fixed

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,5 @@ module.exports={
     //Local MongoDB
     //"URI" : "mongodb://localhost/videogames"
     //MLab ->
-     "URI" : "mongodb://kevinburnside:12345@abc@ds161049.mlab.com:61049/videogames12";
+     "URI" : "mongodb://kevinburnside:12345@abc@ds161049.mlab.com:61049/videogames12"
 }
