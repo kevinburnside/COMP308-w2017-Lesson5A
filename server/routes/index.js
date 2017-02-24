6ee002466c47b4018519dc286df297aebd98a694
kevinburnside <kevinburnside15@hotmail.com>
2017-02-24 13:38:35 -0500

    updated views

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,14 +12,14 @@ router.get('/', (req, res, next) => {
 });
 
 /* GET about page. */
-router.get('/about', (req, res, next) => {
+router.get('/gameslist', (req, res, next) => {
   // find all games in the games collection
   game.find((err, games) => {
     if (err) {
       return console.error(err);
     }
     else {
-      res.render('content/about', {
+      res.render('content/gamelist', {
         title: 'About',
         games: games
       });
